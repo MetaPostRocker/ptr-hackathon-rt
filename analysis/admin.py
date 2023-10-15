220136e34f0c724652a9191117832ad4af1e3255
dooweed <bydev2001@gmail.com>
2023-10-15 11:52:03 +0500

    Add admin for Analysis entry

--- analysis/admin.py
+++ analysis/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from analysis.models import AnalysisTag
+from analysis.models import AnalysisTag, AnalysisEntry
 
 
 @admin.register(AnalysisTag)
 class AnalysisTagAdmin(admin.ModelAdmin):
     list_display = ('name', 'type')
+
+
+@admin.register(AnalysisEntry)
+class AnalysisEntryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'owner', 'tags_joined', 'owner', 'diagnosis')
+
+    @admin.display(description='Tags')
+    def tags_joined(self, obj: AnalysisEntry):
+        return ', '.join(item.name for item in obj.tags.all())
