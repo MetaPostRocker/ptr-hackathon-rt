70688b63d5748529f4e2ace20b4f4bf509557fec
dooweed <bydev2001@gmail.com>
2023-10-15 13:06:46 +0500

    Minor changes

--- medical_tests/urls.py
+++ medical_tests/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
-from medical_tests.views import MedicalCertificateViewSet
+from medical_tests.views import MedicalCertificateViewSet, VaccineViewSet
 
 medical_tests_router = SimpleRouter()
 
-medical_tests_router.register('medical_certificates/medical_tests',
+medical_tests_router.register('medical_tests/medical_certificates',
                               MedicalCertificateViewSet)
+medical_tests_router.register('medical_tests/vaccines',
+                              VaccineViewSet)
